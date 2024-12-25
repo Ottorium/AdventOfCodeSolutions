73d8c8945633bfe65440655dc8607ad0ced44e9b
Michael Ludwig <michael.ludwig1392@gmail.com>
2024-12-25 11:51:42 +0100

    2024 day 25

--- 2024/Day25/main.py
+++ 2024/Day25/main.py
@@ -1,1 +1,34 @@
-# Haven't solved this yet. Good Luck solving it yourself!
+blocks = open("25.txt").read().split("\n\n")
+
+locks = list()
+keys = list()
+for block in blocks:
+    grid = [[s] for s in zip(*block.split("\n")[1:])]
+
+    nums = list()
+    for row in grid:
+        c = 0
+        for s in row[0] if block[0][0] == "#" else row[0][:-1]:
+            c += 1 if s == "#" else 0
+        nums.append(c)
+
+    if block[0][0] == "#":
+        locks.append(nums)
+    else:
+        keys.append(nums)
+
+
+def couldFit(key, lock):
+    for i in range(len(key)):
+        if key[i] + lock[i] > 5:
+            return False
+    return True
+
+s = 0
+for lock in locks:
+
+    for key in keys:
+        if couldFit(key, lock):
+            s += 1
+print("Part 1: " + str(s))
+print("Could not get Part 2 because I didn't solve all of the previous puzzles'")
